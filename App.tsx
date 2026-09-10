5e84e27d36a21b8fb51d08d63e3e0af37998b33c
Caique-Souza1 <caiquesouzalg@gmail.com>
2026-09-10 09:59:53 -0300

    correção

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Compras da semana</Text>
       <Text>Arroz</Text>
       <Text>Feijão</Text>
@@ -13,11 +13,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
